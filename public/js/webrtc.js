fdb72be471ca00f98cc71480b3e7e8af1151d391
FabioHy <fabioharleyfilho@gmail.com>
2026-08-18 17:05:11 -0300

    debug stungoogle

--- public/js/webrtc.js
+++ public/js/webrtc.js
@@ -31,19 +31,8 @@ class WebRTCManager {
         { urls: 'stun:stun3.l.google.com:19302' },
         { urls: 'stun:stun4.l.google.com:19302' },
         { urls: 'stun:stun.cloudflare.com:3478' },
-        { urls: 'stun:global.stun.twilio.com:3478' },
-
-        {
-          urls: [
-            'turn:openrelay.metered.ca:80',
-            'turn:openrelay.metered.ca:443',
-            'turn:openrelay.metered.ca:443?transport=tcp'
-          ],
-          username: 'openrelay',
-          credential: 'openrelay'
-        }
+        { urls: 'stun:global.stun.twilio.com:3478' }
       ],
-
       iceCandidatePoolSize: 10
     };
 
